2a03964762516c6aa7d7662833b31ea7f90b450e
harshsah <harshsah99@gmail.com>
2024-05-10 19:00:45 +0530

    update resolve_velocity

--- core/particle_contact.py
+++ core/particle_contact.py
@@ -11,7 +11,7 @@ class ParticleContact:
     The contact has no callable functions, it just holds the contact details. To resolve a set of contacts, use the
     particle contact resolver class.
 
-    :param paticle: the particles involved in the contact
+    :param particles: the particles involved in the contact
     :param restitution: the coefficient of normal restitution at the contact
     :param contact_normal: the direction of the contact in the world coordinates
     """
@@ -21,6 +21,8 @@ class ParticleContact:
             raise ValueError("Particles cannot be empty")
         if len(particles) >= 2:
             raise ValueError("Particles cannot contain more than two particles")
+        if len(particles) == 1:
+            particles = particles[0], None
         self.particles = particles
         self.restitution = restitution
         self.contact_normal = contact_normal
@@ -49,4 +51,41 @@ class ParticleContact:
         Handles the impulse calculations for this collision.
         :param duration: the duration
         """
-        pass
+
+        # Find the velocity in the direction of the contact.
+        separating_velocity = self._calculate_separating_velocity()
+
+        # Check whether it needs to be resolved.
+        if separating_velocity > 0:
+            # The contact is either separating or stationary - there’s no impulse required.
+            return
+
+        # Calculate the new separating velocity.
+        new_sep_velocity = -separating_velocity * self.restitution
+        delta_velocity = new_sep_velocity - separating_velocity
+
+        # We apply the change in velocity to each object in proportion to its inverse mass
+        # (i.e., those with lower inverse mass [higher actual mass] get less change in velocity).
+        total_inverse_mass = self.particles[0].inverse_mass
+        if self.particles[1]:
+            total_inverse_mass += self.particles[1].inverse_mass
+
+        # If all particles have infinite mass, then impulses have no effect.
+        if total_inverse_mass <= 0:
+            return
+
+        # Calculate the impulse to apply.
+        impulse = delta_velocity / total_inverse_mass
+
+        # Find the amount of impulse per unit of inverse mass.
+        impulse_per_inverse_mass = self.contact_normal * impulse
+
+        # Apply impulses: they are applied in the direction of the contact,
+        # and are proportional to the inverse mass.
+        self.particles[0].velocity = (self.particles[0].velocity
+                                      + impulse_per_inverse_mass * self.particles[0].inverse_mass)
+
+        if self.particles[1]:
+            # Particle 1 goes in the opposite direction.
+            self.particles[1].velocity = (self.particles[1].get_velocity()
+                                          + impulse_per_inverse_mass * -self.particles[1].inverse_mass)
